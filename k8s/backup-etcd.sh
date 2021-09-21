e83ac03dd03f663bcc250063c5ea4b176e4bbe1b
Rob Pabo <rjpabo@gmail.com>
2021-09-21 16:23:46 +0000

    initial commit

---
+++ k8s/backup-etcd.sh
@@ -0,0 +1,5 @@
+ETCDCTL_API=3 etcdctl snapshot save /home/ubuntu/k8s/etcd-backup.db \
+--endpoints=https://127.0.0.1:2379 \
+--cacert=/etc/kubernetes/pki/etcd/ca.crt \
+--cert=/etc/kubernetes/pki/etcd/server.crt \
+--key=/etc/kubernetes/pki/etcd/server.key
